ea9b8479d521d5ee75fef92a06b94b798c146032
0neAnd0nlyKen <kendrickraphael@gmail.com>
2025-05-25 13:50:58 +0800

    isi tabel pengajuan cuti 0 (lupa up update file create tabel untuk migrate)

--- database/migrations/2025_05_24_204233_create_leave_applications_table.php
+++ database/migrations/2025_05_24_204233_create_leave_applications_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('leave_applications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('pegawai_id')->constrained('employees')->onDelete('cascade');
+            $table->date('tgl_mulai');
+            $table->date('tgl_selesai');
+            $table->enum('jenis_cuti', ['tahunan','sakit','melahirkan','izin','alasan_pribadi','lainnya'])->default('tahunan');
+            $table->text('alasan')->nullable();
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->foreignId('pemberi_persetujuan_id')->nullable()->constrained('employees')->onDelete('set null');
+            $table->timestamp('approved_at')->nullable();
             $table->timestamps();
         });
     }
